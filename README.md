b8601aef87e4d9a2ffbef23038f54ef49288be31
rjenkins <rjenkins79@yahoo.com>
2013-06-04 12:19:09 +0200

    temp fix transaction

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Ruby on Rails 4.x Time Banking (LETS) software. Lightweight alternative for [Cyc
 * Out-of-box [Ripple gateway](https://ripple.com) support (trading over communities)
 * Easy and free heroku deployment
 * Mobile payment support
-* consistent API
+* Consistent API
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ActiveRecord::Base
-  belongs_to :account, class_name: 'Account', foreign_key: 'account_to'
   belongs_to :account, class_name: 'Account', foreign_key: 'account_from'
+  # belongs_to :account, class_name: 'Account', foreign_key: 'account_to'
   attr_accessible :account_from, :accounts_to, :amount, :notes
   attr_accessor :user_to
 end
