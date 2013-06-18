78d2ff8ddd798fcbb1d70d49cb45151d74e5ee6d
rjenkins <rjenkins79@yahoo.com>
2013-06-18 17:18:58 +0200

    add invite only option fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
-  devise :registerable if ENV['invite_only'] == 0
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :trackable, :validatable
+  devise :registerable if ENV['INVITE_ONLY'] == '0'
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Gmapp::Application.routes.draw do
-  devise_for :users, skip: :registrations
-  devise_scope :user do
-    resource :registration,
-      only: [:new, :create, :edit, :update],
-      path: 'users',
-      path_names: { new: 'sign_up' },
-      controller: 'devise/registrations',
-      as: :user_registration do
-        get :cancel
-      end
-  end
+  devise_for :users
   resources :users do
     get :ads
   end
